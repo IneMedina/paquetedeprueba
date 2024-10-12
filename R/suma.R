eae27cea8d5775582245fe73bca8d8286da78137
PersonalAccessToken <inesmed004@gmail.com>
2024-10-12 16:00:52 -0300

    Documenta funcion suma

--- R/suma.R
+++ R/suma.R
@@ -1,7 +1,22 @@
-#suma dos numeros
 
+#' Suma dos numeros
+#'
+#' La funcion `suma()` toma 2 números positivos y los suma.
+#'
+#' Estos son detalles extras de la funcion.
+#'
+#' @param x un vector numérico
+#' @param y un vector numérico
+#'
+#' @return
+#' vector númerico con la suma de `x` e `y`.
+#'
+#' @examples
+#' sume(2, 2)
+#' suma(1, 10)
+#'
+#' @export
 suma <- function(x, y) {
  return(x + y)
 }
 
-#control + shef +L (carga funciones de 0, cargamos el paquete, no instalamos)
--- data_raw/crea_datos.R
+++ data_raw/crea_datos.R
@@ -6,3 +6,7 @@ datos <- data.frame(a = sample(1000),
                     b = sample(1000)*5)
 
 usethis::use_data(datos) #lo incluimos a los datos dentro de nuestro paquete
+
+
+use_git_config(user.name = "PersonalAccessToken",
+               user.email = "inesmed004@gmail.com")
