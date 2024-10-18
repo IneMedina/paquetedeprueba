9a42624949f8052c2762454aec85083fde31628b
PersonalAccessToken <inesmed004@gmail.com>
2024-10-18 18:18:51 -0300

    Agrego documentacion del conjunto de datos.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' conjunto de datos de ejemplo
+#'
+#' Los datos son utlilizados como ejemplo de nuestra funcion de suma.
+#'
+#' @format Los datos son generados de forma sintetica.
+#' \describe{
+#'  \item{a}{muestra de datos}
+#'  \item{b}{muestra de datos multiplicada por cinco}
+#' }
+#' @source generacion propia con funciones de R
+"datos"
